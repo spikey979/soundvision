c982ab3701d16a0503d479f119ee9fc0bb47e348
Kristijan Smrekar <kristijan.smrekar@sensorium.hr>
2023-07-20 13:09:43 +0200

    update

--- server.py
+++ server.py
@@ -1,14 +1,15 @@
 import os
 import threading
 from flask import Flask, render_template, send_file
-#from pyngrok import ngrok
+
+from pyngrok import ngrok
 
 os.environ["FLASK_ENV"] = "development"
 app = Flask(__name__)
 port = 5000
-#ngrok.set_auth_token("2Smrl6JUM9rTUwh3nuGmkKYnRwp_67SzH3fGPUbScrvPteVmC")
-#public_url = ngrok.connect(port).public_url
-#print("ovo je public_url", public_url)
+ngrok.set_auth_token("2Smrl6JUM9rTUwh3nuGmkKYnRwp_67SzH3fGPUbScrvPteVmC")
+public_url = ngrok.connect(port).public_url
+print("ovo je public_url", public_url)
 print("ovo je port", port)
 
 #app.config["BASE_URL"] = public_url
@@ -22,7 +23,7 @@ def index():
     #return str(e)
 
 # samo lokalno
-app.run(debug=True)
+#app.run(debug=True)
 
 # Start the Flask server in a new thread
-#threading.Thread(target=app.run, kwargs={"use_reloader": False}).start()
+threading.Thread(target=app.run, kwargs={"use_reloader": False}).start()
